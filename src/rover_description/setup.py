c5178aa898f1051fffc9236f0cc00bd6aed0e9ce
highdreameater <vitthalseth553@gmail.com>
2025-10-21 19:51:35 +0000

    Task 1: RViz simulation for rover_description

---
+++ src/rover_description/setup.py
@@ -0,0 +1,41 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'rover_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        # This is the important part!
+        # Install all .launch.py files from the 'launch' directory
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+
+        # Install our .rviz config file
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.rviz')),
+
+        # Install all .urdf files from the 'urdf' directory
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+
+        # Install all .STL files from the 'meshes' directory
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*.STL')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='your_email@todo.com',
+    description='Rover description and control package',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # If you create any Python nodes, add them here
+        ],
+    },
+)
